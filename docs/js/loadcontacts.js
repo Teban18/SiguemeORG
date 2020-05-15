257d1bd2095b9fb797d33dfadce04b05de7cd2c2
Teban18 <aepr_18_@hotmail.com>
2020-05-14 20:53:43 -0500

    added dinamic contact links
    next step: add non-functional requirements to the user interface.

---
+++ docs/js/loadcontacts.js
@@ -0,0 +1,18 @@
+
+//Funcion que valida la imagen que se está mostrando
+
+function setContactLink()
+{
+    if(rotarImagenes()==0)
+    {
+     document.getElementById("inicon").href = "https://www.linkedin.com/in/andr%C3%A9s-esteban-posada-rinc%C3%B3n-94424b1a7/";
+     document.getElementById("ghicon").href = "https://github.com/Teban18";
+    }
+    else
+    {
+       document.getElementById("inicon").href = "https://www.linkedin.com/in/marcela-bula-torres-416401157/";
+       document.getElementById("ghicon").href = "https://github.com/MarcelaBulaT";
+    }
+}
+
+
--- docs/js/resume.js
+++ docs/js/resume.js
@@ -1,13 +1,18 @@
-(function($) {
+(function($) 
+{
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() 
+  {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) 
+    {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
+      if (target.length) 
+      {
+        $('html, body').animate
+        ({
           scrollTop: (target.offset().top)
         }, 1000, "easeInOutExpo");
         return false;
@@ -16,12 +21,14 @@
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
+  $('.js-scroll-trigger').click(function() 
+  {
     $('.navbar-collapse').collapse('hide');
   });
 
   // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
+  $('body').scrollspy
+  ({
     target: '#sideNav'
   });
 
