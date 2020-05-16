49de15232419d8edfdacde67c3814bd63c9a43a9
Teban18 <aepr_18_@hotmail.com>
2020-05-15 20:57:25 -0500

    added improvement of responsive view

--- docs/js/loadimages.js
+++ docs/js/loadimages.js
@@ -2,7 +2,7 @@
 
 //Array con las imagenes que se iran mostrando en la web
     
-var imagenes = 
+var images = 
 [
   'https://scontent.fbog2-1.fna.fbcdn.net/v/t1.0-9/p960x960/96220846_130373231953648_8237748583693025280_o.jpg?_nc_cat=101&_nc_sid=dd7718&_nc_eui2=AeHxmY_29KPXRuv7mmYKkQG8tNQVkwMBCI601BWTAwEIjokQcZ_iHmVE6brUR_o1jpetPnQXGFHUlP--ETZcnhS7&_nc_ohc=tXnFiMEc4mgAX8Ty-B6&_nc_ht=scontent.fbog2-1.fna&_nc_tp=6&oh=c87514a6de76d4a86d46fea80f7c83bf&oe=5EE0B2C7',
   'https://pps.whatsapp.net/v/t61.24694-24/95090693_653973981846514_737736060880437560_n.jpg?oe=5EC0E25C&oh=11e8d4ed40242782f3ba75e86be2d40c'
@@ -12,8 +12,8 @@ var imagenes =
     
    function rotateImage()
    {
-       var index=Math.floor((Math.random()*imagenes.length));
-       document.getElementById("img").src=imagenes[index];
+       var index=Math.floor((Math.random()*images.length));
+       document.getElementById("img").src=images[index];
        return index;
    }
       
@@ -24,8 +24,8 @@ var imagenes =
     {
         rotateImage();
         setContactLink();
-        setInterval(rotateImage(),20000);
-        setInterval(setContactLink(),20000);
+        setInterval(rotateImage(),5000);
+        setInterval(setContactLink(),5000);
     }
    
    
