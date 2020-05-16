0a8621d4dfb1c3a4a0ed3aaa2be13493d48d1877
Teban18 <aepr_18_@hotmail.com>
2020-05-16 00:45:00 -0500

    added manually change photo and contact links
    next step, add information completely

--- docs/js/loadcontacts.js
+++ docs/js/loadcontacts.js
@@ -1,17 +1,15 @@
 
 //Funcion que valida la imagen que se está mostrando
 
-function setContactLink()
+function setContactLink(data)
 {
-    if(rotateImage()==0)
+    if(data=='teban')
     {
-        console.log(rotateImage());
      document.getElementById("inicon").href = "https://www.linkedin.com/in/andr%C3%A9s-esteban-posada-rinc%C3%B3n-94424b1a7/";
      document.getElementById("ghicon").href = "https://github.com/Teban18";
     }
     else
     {
-        console.log(rotateImage());
        document.getElementById("inicon").href = "https://www.linkedin.com/in/marcela-bula-torres-416401157/";
        document.getElementById("ghicon").href = "https://github.com/MarcelaBulaT";
     }
--- docs/js/loadimages.js
+++ docs/js/loadimages.js
@@ -3,29 +3,25 @@
 //Array con las imagenes que se iran mostrando en la web
     
 var images = 
-[
-  'https://scontent.fbog2-1.fna.fbcdn.net/v/t1.0-9/p960x960/96220846_130373231953648_8237748583693025280_o.jpg?_nc_cat=101&_nc_sid=dd7718&_nc_eui2=AeHxmY_29KPXRuv7mmYKkQG8tNQVkwMBCI601BWTAwEIjokQcZ_iHmVE6brUR_o1jpetPnQXGFHUlP--ETZcnhS7&_nc_ohc=tXnFiMEc4mgAX8Ty-B6&_nc_ht=scontent.fbog2-1.fna&_nc_tp=6&oh=c87514a6de76d4a86d46fea80f7c83bf&oe=5EE0B2C7',
-  'https://pps.whatsapp.net/v/t61.24694-24/95090693_653973981846514_737736060880437560_n.jpg?oe=5EC0E25C&oh=11e8d4ed40242782f3ba75e86be2d40c'
-];
+{
+  'teban':{'link':'https://scontent.fbog2-1.fna.fbcdn.net/v/t1.0-9/p960x960/96220846_130373231953648_8237748583693025280_o.jpg?_nc_cat=101&_nc_sid=dd7718&_nc_eui2=AeHxmY_29KPXRuv7mmYKkQG8tNQVkwMBCI601BWTAwEIjokQcZ_iHmVE6brUR_o1jpetPnQXGFHUlP--ETZcnhS7&_nc_ohc=tXnFiMEc4mgAX8Ty-B6&_nc_ht=scontent.fbog2-1.fna&_nc_tp=6&oh=c87514a6de76d4a86d46fea80f7c83bf&oe=5EE0B2C7','name':'teban'},
+  'marce':{'link':'https://pps.whatsapp.net/v/t61.24694-24/95090693_653973981846514_737736060880437560_n.jpg?oe=5EC0E25C&oh=11e8d4ed40242782f3ba75e86be2d40c','name':'marce'}
+};
    
-   //Funcion para cambiar la imagen de manera aleatoria
-    
-   function rotateImage()
-   {
-       var index=Math.floor((Math.random()*images.length));
-       document.getElementById("img").src=images[index];
-       return index;
+   //Funcion que valida accion onmouseover sobre el nombre esteban
+  document.getElementById("esteban").onmouseover=function()
+  {
+    document.getElementById("img").src=images.teban.link;
+    setContactLink(images.teban.name);
+  }
+  //Funcion que valida accion onmouseover sobre el nombre marcela
+  document.getElementById("marcela").onmouseover=function()
+  {
+    document.getElementById("img").src=images.marce.link;
+    setContactLink(images.marce.name);
+  }
+ 
+   onload=function(){
+     document.getElementById("img").src=images.marce.link;
+     setContactLink(images.marce.name);
    }
-      
-    //Función que se ejecuta una vez cargada la página
-    //carga link de contacto e imagenes
-    
-    onload=function()
-    {
-        rotateImage();
-        setContactLink();
-        setInterval(rotateImage(),5000);
-        setInterval(setContactLink(),5000);
-    }
-   
-   
