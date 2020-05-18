555fe65df5559599437e01d6cec53281d9b0651e
Teban18 <aepr_18_@hotmail.com>
2020-05-17 22:18:55 -0500

    Added functionalities

--- docs/js/loadimages.js
+++ docs/js/loadimages.js
@@ -1,10 +1,10 @@
  //Funcion que valida accion onmouseover sobre nombres
 
  function validation(person)
- {    
- document.getElementById(person.name).onclick=function()
- {
-  document.getElementById("inicon").href = person.linkedin;
-  document.getElementById("ghicon").href = person.github;
- }
+ {  
+    document.getElementById(person.name).onclick=function()
+    {
+        document.getElementById("inicon").href = person.linkedin;
+        document.getElementById("ghicon").href = person.github;
+    }
  }
--- docs/js/loadresources.js
+++ docs/js/loadresources.js
@@ -3,16 +3,9 @@
 /*Funcion que se invoca una vez cargado el documento 
 carga el index con los datos de data.json y person.json
 */
+
 $(document).ready(function ()
 {
-  $.getJSON("json/data.json", function( json )
-  {        
-    let data = json.textdata;        
-    for(i in data)
-    {                    
-      writeData(data[i]);   
-    }          
-  });
   $.getJSON("json/person.json", function( json )
   {
     let person = json.person;
@@ -20,5 +13,13 @@ $(document).ready(function ()
     {
       validation(person[j]);
     }
+  });
+  $.getJSON("json/data.json", function( json )
+  {        
+    let data = json.textdata;        
+    for(i in data)
+    {                    
+      writeData(data[i]);   
+    }          
   });
   }); 
